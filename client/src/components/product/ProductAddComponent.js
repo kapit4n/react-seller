86ea374427f82aefac5f06d0d921ddd77f8b82e2
larce <luis.arce22@gmail.com>
2017-01-28 19:23:59 -0400

    Adding the fields for the add product form

--- client/src/components/product/ProductAddComponent.js
+++ client/src/components/product/ProductAddComponent.js
@@ -3,27 +3,45 @@
 import React from 'react';
 
 require('styles/product/ProductAdd.css');
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonToolbar, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
 
 class ProductAddComponent extends React.Component {
   render() {
     return (
       <div className="productadd-component">
         <ButtonToolbar>
-          <Button>Default</Button>
-             {/* Indicates a successful or positive action */}
-          <Button bsStyle="success">Success</Button>
-
-    {/* Contextual button for informational alert messages */}
-    <Button bsStyle="info">Info</Button>
-
-    {/* Indicates caution should be taken with this action */}
-    <Button bsStyle="warning">Warning</Button>
-
-    {/* Indicates a dangerous or potentially negative action */}
-    <Button bsStyle="danger">Danger</Button>
-
+          <Button>Save</Button>
+          <Button bsStyle="danger">Cancel</Button>
         </ButtonToolbar>
+        <FormGroup controlId="formName">
+          <ControlLabel>Name</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <FormGroup controlId="formCode">
+          <ControlLabel>Code</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <FormGroup controlId="form">
+          <ControlLabel>Code</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <FormGroup controlId="formCode">
+          <ControlLabel>Price</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <FormGroup controlId="formCode">
+          <ControlLabel>Stock</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <FormGroup controlId="formCode">
+          <ControlLabel>Description</ControlLabel>
+          <FormControl type="text"placeholder="Enter text"/>
+        </FormGroup>
+        <ButtonToolbar>
+          <Button>Save</Button>
+          <Button bsStyle="danger">Cancel</Button>
+        </ButtonToolbar>
+
       </div>
     );
   }
