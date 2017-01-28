f15fd39ed731281afbe7df3dfd135cbbc9472fc9
larce <luis.arce22@gmail.com>
2017-01-28 19:04:09 -0400

    Updated the project to use boostrap components in the rectjs components

--- client/src/components/product/ProductAddComponent.js
+++ client/src/components/product/ProductAddComponent.js
@@ -3,12 +3,27 @@
 import React from 'react';
 
 require('styles/product/ProductAdd.css');
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
 class ProductAddComponent extends React.Component {
   render() {
     return (
       <div className="productadd-component">
-        Please edit src/components/product//ProductAddComponent.js to update this component!
+        <ButtonToolbar>
+          <Button>Default</Button>
+             {/* Indicates a successful or positive action */}
+          <Button bsStyle="success">Success</Button>
+
+    {/* Contextual button for informational alert messages */}
+    <Button bsStyle="info">Info</Button>
+
+    {/* Indicates caution should be taken with this action */}
+    <Button bsStyle="warning">Warning</Button>
+
+    {/* Indicates a dangerous or potentially negative action */}
+    <Button bsStyle="danger">Danger</Button>
+
+        </ButtonToolbar>
       </div>
     );
   }
