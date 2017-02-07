64da5d6a9faa5234162901a8cc61b3e1187e1132
larce <luis.arce22@gmail.com>
2017-02-06 22:50:58 -0400

    Updating navbar with boostrap style

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,21 +3,42 @@ require('styles/App.css');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Nav, NavItem,Navbar,MenuItem,NavDropdown,    Table, Image, Button, Grid, Row, Col} from 'react-bootstrap';
 
 let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
+  
   render() {
     return (
-         <div>
-            <ul>
-               <li><a href="/home">Home</a> </li>
-               <li><a href="/about">About</a> </li>
-               <li><a href="/contact">Contact</a> </li>
-               <li><a href="/product-list">ProductList</a> </li>
-            </ul>
-           {this.props.children}
-         </div>
+      <div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="/home">Home</NavItem>
+            <NavItem eventKey={2} href="/about">About</NavItem>
+            <NavDropdown eventKey={3} title="Products" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1} href="/product-list">List</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Facebook</NavItem>
+            <NavItem eventKey={2} href="#">Login</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+       {this.props.children}
+      </div>
       )
   }
 }
