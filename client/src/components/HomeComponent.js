c7a54d2c8fa344a473e083dab5942c490f47fb62
larce <luis.arce22@gmail.com>
2017-02-18 12:02:35 -0400

    Adding modal to insert the quantity of the product to be added in the shopping card

--- client/src/components/HomeComponent.js
+++ client/src/components/HomeComponent.js
@@ -2,12 +2,17 @@
 
 import React from 'react';
 require('styles//Home.css');
-import {Grid, Row, Col, Image, Button, ButtonToolbar, Glyphicon} from 'react-bootstrap';
+import {Grid, Row, Col, Image, Button, ButtonToolbar, Glyphicon, Modal, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 
 class HomeComponent extends React.Component {
-
-  handleClick = (productId: any) => {
-    let item = {quantity: 20, price: 5, totalPrice: 100, discount: 0, product: {id: productId}};
+  handleClick = () => {
+    let item = {
+                  quantity: this.state.quantity,
+                  price: this.state.product.price,
+                  totalPrice: this.state.product.price * this.state.quantity,
+                  discount: 0,
+                  product: {id: this.state.product.id}
+                };
     fetch(this.orderURL + '?access_token=' + this.access_token, {
       method: 'POST',
       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', },
@@ -15,6 +20,14 @@ class HomeComponent extends React.Component {
     }).then((response) => response.json())
     .then((responseJson) => { console.log("Hello I saved "); })
     .catch((error) => { console.error(error); });
+    this.setState({ quantity: 1});
+    this.setState({ product: product});
+  };
+
+  handleClickBefore = (product: any) => {
+    this.setState({ show: true});
+    this.setState({ product: product});
+
   };
 
   constructor(){
@@ -22,7 +35,7 @@ class HomeComponent extends React.Component {
     this.productURL = 'http://localhost:3000/api/products';
     this.orderURL = 'http://localhost:3000/api/orderDetails';
     this.access_token = 'T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7';
-    this.state = { products: [] };
+    this.state = {products: [], quantity: 0, show: false, product: {}};
   }
 
   componentDidMount() {
@@ -32,7 +45,15 @@ class HomeComponent extends React.Component {
       .catch((error) => { console.error(error); });
   }
 
+  handleChangeQuantity = (event) => {
+    this.setState({ quantity: event.target.value });
+  }
+
   render() {
+    let close = () => {
+      this.setState({ show: false});
+      this.handleClick();
+    };
     return (
       <div className="home-component">
         <Grid>
@@ -42,12 +63,37 @@ class HomeComponent extends React.Component {
                 <Image width={300} height={300} src={product.img} thumbnail />
                 <Button xs={12} md={12}  bsStyle="link" href={'product-show/' + product.id}>{product.name}</Button>
                 <ButtonToolbar>
-                  <Button onClick={()=>this.handleClick(product.id)} style={{width: 250, marginLeft: 25}}><Glyphicon glyph="shopping-cart"/> Add to Card </Button>
+                  <Button onClick={()=>this.handleClickBefore(product)} style={{width: 250, marginLeft: 25}}><Glyphicon glyph="shopping-cart"/> Add to Card </Button>
                 </ButtonToolbar>
               </Col>;
             }, this)}
           </Row>
         </Grid>
+
+        <Modal show={this.state.show} onHide={close} container={this} aria-labelledby="contained-modal-title">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">Adding to Shopping Card</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={6} md={4} height={60}>
+                  <Image width={60} height={60} src={this.state.product.img} thumbnail />
+                  {this.state.product.name}
+                </Col>;
+              </Row>
+            </Grid>
+            <FormGroup controlId = "formCode">
+                <ControlLabel>Quantity</ControlLabel>
+                <FormControl type = "text" placeholder = "Enter quantity"
+                value = { this.state.quantity } onChange = { this.handleChangeQuantity } />
+            </FormGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={close}>Save</Button>
+          </Modal.Footer>
+        </Modal>
+
       </div>
     );
   }
