419ceafb52b5a898dc4be7253b9b8bd6cc152f60
larce <luis.arce22@gmail.com>
2017-08-05 22:06:14 -0400

    Adding component to show related products

---
+++ client/src/components/product/ProductRelatedComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/product/ProductRelated.css');
+
+class ProductRelatedComponent extends React.Component {
+  render() {
+    return (
+      <div className="productrelated-component">
+        Please edit src/components/product//ProductRelatedComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ProductRelatedComponent.displayName = 'ProductProductRelatedComponent';
+
+// Uncomment properties you need
+// ProductRelatedComponent.propTypes = {};
+// ProductRelatedComponent.defaultProps = {};
+
+export default ProductRelatedComponent;
