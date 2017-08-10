e5b86f536bda45a7c980e49a29dbc7aae2b0bc58
larce <luis.arce22@gmail.com>
2017-08-10 08:20:21 -0400

    Updated card list page to show orders

--- client/src/components/card/CardListComponent.js
+++ client/src/components/card/CardListComponent.js
@@ -5,10 +5,35 @@ import React from 'react';
 require('styles/card/CardList.css');
 
 class CardListComponent extends React.Component {
+  constructor(){
+    super();
+    this.orderURL = 'http://localhost:3000/api/orders';
+    this.access_token = 'T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7';
+    this.state = { orders: []};
+    this.loadItems();
+  }
+
+  loadItems() {
+    fetch(this.orderURL + '?access_token=' + this.access_token) 
+      .then((response) => response.json())
+      .then((responseJson) => { 
+        this.setState({orders: responseJson});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="cardlist-component">
-        Please edit src/components/card//CardListComponent.js to update this component!
+        <ol>
+          {
+            this.state.orders.map(function(order){
+              return <li>order.createdDate</li>;
+            })
+          }
+        </ol>
       </div>
     );
   }
