25ad00515f169458d3a409b1798f799729c07eb1
larce <luis.arce22@gmail.com>
2017-08-16 21:57:57 -0400

    Fixed api URL to get order details related to order

--- client/src/components/card/CardShowComponent.js
+++ client/src/components/card/CardShowComponent.js
@@ -1,21 +1,40 @@
 'use strict';
 
 import React from 'react';
+import { ListGroup, ListGroupItem, Badge, Image, Button, Glyphicon} from 'react-bootstrap';
 
 require('styles/card/CardShow.css');
 
 class CardShowComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.orderDetails = "http://localhost:3000/api/orderDetails?filter[where][orderId][eq]=" + this.props.params.id + "&filter[include]=product";
+    this.orderDetails = "http://localhost:3000/api/orderDetails?filter[where][orderId]=" + this.props.params.id + "&filter[include]=product";
     this.access_token = 'T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7';
     this.state = { orderDetails: [] };
     this.loadItems();
   }
+
+  deliverOrder(id){
+    console.log("TODO deliver order");
+  }
+
+  cancelOrder(id){
+    console.log("TODO cancel order");
+  }
+
   render() {
     return (
       <div className="cardshow-component">
-        Please edit src/components/card//CardShowComponent.js to update this component!
+        <ListGroup>
+          {
+            this.state.orderDetails.map(function(orderDetail){
+              let res = <ListGroupItem><Image src={orderDetail.product.img} thumbnail width={100} />Name: {orderDetail.product.name}<Badge>Quantity: {orderDetail.quantity}</Badge><Badge>Price: ${orderDetail.totalPrice}</Badge></ListGroupItem>;
+              return res;
+            })
+          }
+        </ListGroup>
+        <Button onClick = {()=>this.deliverOrder(order.id)}> <Glyphicon glyph="send"/> Deliver</Button>
+        <Button onClick = {()=>this.cancelOrder(order.id)}> <Glyphicon glyph="remove-sign"/> Remove</Button>
       </div>
     );
   }
@@ -31,7 +50,6 @@ class CardShowComponent extends React.Component {
         console.error(error);
       });
   }
-
 }
 
 CardShowComponent.displayName = 'CardCardShowComponent';
