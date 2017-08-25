ff2625eb48a5df457e23acf61ccc3ce51cc2a279
larce <luis.arce22@gmail.com>
2017-08-25 08:14:36 -0400

    Added customer line item

---
+++ client/src/components/customer/CustomerLineItemComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/customer/CustomerLineItem.css');
+
+class CustomerLineItemComponent extends React.Component {
+  render() {
+    return (
+      <div className="customerlineitem-component">
+        Please edit src/components/customer//CustomerLineItemComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+CustomerLineItemComponent.displayName = 'CustomerCustomerLineItemComponent';
+
+// Uncomment properties you need
+// CustomerLineItemComponent.propTypes = {};
+// CustomerLineItemComponent.defaultProps = {};
+
+export default CustomerLineItemComponent;
