cbbc6c8fa06287c245e91e867bffdbf4880990c8
larce <luis.arce22@gmail.com>
2017-09-02 17:44:32 -0400

    Example of custom endpoint on the loopback server

--- server/common/models/product.js
+++ server/common/models/product.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Product) {
-
+    Product.getName = function(productId, cb) {
+        Product.findById( productId, function (err, instance) {
+            var response = "Name of product is " + instance.name;
+            cb(null, response);
+            console.log(response);
+        });
+      }
+    Product.remoteMethod (
+        'getName',
+        {
+          http: {path: '/getname', verb: 'get'},
+          accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
+          returns: {arg: 'name', type: 'string'}
+        }
+    );
 };
