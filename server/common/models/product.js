0fb474bf2cea56842f2347c0d9261f1ffde637a0
larce <luis.arce22@gmail.com>
2017-09-04 07:23:59 -0400

    Api method to update stock of product

--- server/common/models/product.js
+++ server/common/models/product.js
@@ -1,19 +1,40 @@
 'use strict';
 
-module.exports = function(Product) {
-    Product.getName = function(productId, cb) {
-        Product.findById( productId, function (err, instance) {
+module.exports = function (Product) {
+    Product.getName = function (productId, cb) {
+        Product.findById(productId, function (err, instance) {
             var response = "Name of product is " + instance.name;
             cb(null, response);
-            console.log(response);
         });
-      }
-    Product.remoteMethod (
+    }
+    Product.updateStock = function (productId, amount, cb) {
+        Product.findById(productId, function (err, instance) {
+            var response = "false";
+            if (amount <= instance.stock) {
+                instance.stock = instance.stock - amount;
+                instance.save();
+                response = "true";
+            }
+            cb(null, response);
+        });
+    }
+    Product.remoteMethod(
         'getName',
         {
-          http: {path: '/getname', verb: 'get'},
-          accepts: {arg: 'id', type: 'string', http: { source: 'query' } },
-          returns: {arg: 'name', type: 'string'}
+            http: { path: '/getname', verb: 'get' },
+            accepts: { arg: 'id', type: 'string', http: { source: 'query' } },
+            returns: { arg: 'name', type: 'string' }
+        }
+    );
+    Product.remoteMethod(
+        'updateStock',
+        {
+            http: { path: '/updatestock', verb: 'get' },
+            accepts: [
+                { arg: 'id', type: 'string', http: { source: 'query' } },
+                { arg: 'amount', type: 'string', http: { source: 'query' } },
+            ],
+            returns: { arg: 'success', type: 'string' }
         }
     );
 };
