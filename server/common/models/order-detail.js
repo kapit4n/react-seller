a7cfd12881ccc61da856da9a0851209555cf445c
larce <luis.arce22@gmail.com>
2017-10-11 21:51:01 -0400

    Added endpoint to get current card total price

--- server/common/models/order-detail.js
+++ server/common/models/order-detail.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Orderdetail) {
-
+  Orderdetail.getCurretTotal = function(cb) {
+    Orderdetail.find({}, function(err, instance) {
+      var response = 0;
+      instance.forEach(function(orderItem) {
+        if (!orderItem.orderId) {
+          response += orderItem.totalPrice;
+        }
+      });
+      cb(null, response);
+    });
+  }
+  Orderdetail.remoteMethod(
+    'getCurretTotal', {
+      http: { path: '/currentTotal', verb: 'get' },
+      returns: { arg: 'total', type: 'string' }
+    }
+  );
 };
