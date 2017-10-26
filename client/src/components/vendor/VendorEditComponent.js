559c474c5d16f4eed632c22b9e0996bac63b573f
larce <luis.arce22@gmail.com>
2017-10-26 06:00:03 -0400

    Renamed files

---
+++ client/src/components/vendor/VendorEditComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/vendor/VendorEdit.css');
+
+class VendorEditComponent extends React.Component {
+  render() {
+    return (
+      <div className="vendoredit-component">
+        Please edit src/components/vendor//VendorEditComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+VendorEditComponent.displayName = 'VendorVendorEditComponent';
+
+// Uncomment properties you need
+// VendorEditComponent.propTypes = {};
+// VendorEditComponent.defaultProps = {};
+
+export default VendorEditComponent;
---
+++ client/src/components/vendor/VendorShowComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/vendor/VendorShow.css');
+
+class VendorShowComponent extends React.Component {
+  render() {
+    return (
+      <div className="vendorshow-component">
+        Please edit src/components/vendor//VendorShowComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+VendorShowComponent.displayName = 'VendorVendorShowComponent';
+
+// Uncomment properties you need
+// VendorShowComponent.propTypes = {};
+// VendorShowComponent.defaultProps = {};
+
+export default VendorShowComponent;
