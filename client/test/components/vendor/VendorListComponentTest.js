52cf871e5684db8f8895db56a243f85ccb5a5db7
Luis Arce <kapit4n@users.noreply.github.com>
2018-03-27 12:44:24 -0400

    Updated VendorListComponent scenarios to test

--- client/test/components/vendor/VendorListComponentTest.js
+++ client/test/components/vendor/VendorListComponentTest.js
@@ -12,11 +12,17 @@ import VendorListComponent from 'components/vendor/VendorListComponent.js';
 describe('VendorListComponent', () => {
   let component;
 
+  /** Load vendors */
   beforeEach(() => {
     component = createComponent(VendorListComponent);
   });
 
   it('should have its component name as default className', () => {
     expect(component.props.className).to.equal('vendorlist-component');
+  });
+  
+  it('should list the vendors on the API', () => {
+    // query the table that holds the vendors
+    // count and review that there is more than one vendor
   });
 });
