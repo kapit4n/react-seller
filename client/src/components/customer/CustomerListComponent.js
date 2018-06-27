b46793d7933388320cd336495007331ae0f5fdcd
lartl <luis.arce22@gmail.com>
2018-06-26 22:02:52 -0400

    Remove a customer record

--- client/src/components/customer/CustomerListComponent.js
+++ client/src/components/customer/CustomerListComponent.js
@@ -8,45 +8,89 @@ import CustomerLineItemComponent from './CustomerLineItemComponent';
 
 
 class CustomerListComponent extends React.Component {
-	constructor() {
+  constructor() {
     super();
-    this.customerURL = 'http://localhost:3000/api/customers';
-    this.access_token = 'T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7';
-    this.state = { customers: []};
+    this.customerURL = "http://localhost:3000/api/customers";
+    this.access_token =
+      "T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7";
+    this.state = { customers: [] };
   }
   componentDidMount() {
-    fetch(this.customerURL + '?access_token=' + this.access_token) 
-      .then((response) => response.json())
-      .then((responseJson) => { this.setState({customers:responseJson});})
-      .catch((error) => { console.error(error); });
+    this.loadCustomers();
+  }
+
+  loadCustomers = () => {
+    fetch(this.customerURL + "?access_token=" + this.access_token)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ customers: responseJson });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   handleAdd = () => {
-    browserHistory.push('/customer-add/');
+    browserHistory.push("/customer-add/");
+  };
+
+  removeCustomer = id => {
+    let headers = { method: "DELETE", headers: { Accept: "application/json", "Content-Type": "application/json" } };
+    fetch(this.customerURL + "/" + id + "?access_token=" + this.access_token, headers )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.loadCustomers();
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   render() {
     return (
       <div>
         <Grid>
-          <Button onClick = { this.handleAdd }><Glyphicon glyph="plus"/></Button>
-          <img id="preview" style={{width: 300, position: 'absolute', left: '30%', top: '15%'}}/>
+          <Button onClick={this.handleAdd}>
+            <Glyphicon glyph="plus" />
+          </Button>
+          <img
+            id="preview"
+            style={{
+              width: 300,
+              position: "absolute",
+              left: "30%",
+              top: "15%"
+            }}
+          />
           <Table striped bordered condensed hover>
             <thead>
               <tr>
                 <th>Name</th>
                 <th>Code</th>
                 <th>Address</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {this.state.customers.map(function (customer) {
-                return  <CustomerLineItemComponent key={customer.id} customer={customer}/>;
-                })
-              }
+              {this.state.customers.map( customer => {
+                return <tr className={"customerlineitem-component"}>
+                    <td>
+                      <a href={"customer-show/" + customer.id}>
+                        {customer.name}
+                      </a>
+                    </td>
+                    <td>{customer.budget}</td>
+                    <td>{customer.address}</td>
+                    <td>
+                      <Button onClick={()=>this.removeCustomer(customer.id)} bsStyle="danger">
+                        <Glyphicon glyph="remove" />
+                      </Button>
+                    </td>
+                  </tr>;
+              })}
             </tbody>
           </Table>
-          </Grid>
+        </Grid>
       </div>
     );
   }
