6eb336da0bf84a870fd371949e408da6286e3925
lartl <luis.arce22@gmail.com>
2018-06-26 21:40:36 -0400

    Updated cart list page to remove an order

--- client/src/components/cart/CartListComponent.js
+++ client/src/components/cart/CartListComponent.js
@@ -1,44 +1,77 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import { ListGroup, ListGroupItem, Badge, Button, Grid} from 'react-bootstrap';
+import React from "react";
+import {
+  ListGroup,
+  ListGroupItem,
+  Badge,
+  Button,
+  Grid,
+  Glyphicon
+} from "react-bootstrap";
 
-require('styles/cart/CartList.css');
+require("styles/cart/CartList.css");
 
 class CartListComponent extends React.Component {
   constructor() {
     super();
-    this.orderURL = 'http://localhost:3000/api/orders';
-    this.access_token = 'T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7';
-    this.state = { orders: []};
+    this.orderURL = "http://localhost:3000/api/orders";
+    this.access_token =
+      "T4SH5NkUULeFPSLEXhycyMvt0HMNINxTdOvYjGzGZkxvMmKZeJbne4TdJfcDLAr7";
+    this.state = { orders: [] };
     this.loadItems();
   }
 
-  loadItems() {
-    fetch(this.orderURL + '?filter[include]=customer&access_token=' + this.access_token)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({orders: responseJson});
+  loadItems = () => {
+    fetch(
+      this.orderURL +
+        "?filter[include]=customer&access_token=" +
+        this.access_token
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ orders: responseJson });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
-  }
+  };
+
+  removeOrder = id => {
+    fetch(this.orderURL + "/" + id + "?access_token=" + this.access_token, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        this.loadItems();
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
   render() {
     return (
       <div className="cartlist-component">
         <Grid>
           <ListGroup>
-            {
-              this.state.orders.map(function(order) {
-                let res = <ListGroupItem><Button bsStyle="info" href={'../cart-show/' + order.id}>SHOW</Button> <Button bsStyle="danger" href={'../cart-show/' + order.id}>CANCEL</Button> {order.description}, customer: {order.customer ? order.customer.name: ""}<Badge>{order.createdDate}</Badge><Badge>Total: {order.total}</Badge></ListGroupItem>;
-                if (order.delivered) {
-                  res = <ListGroupItem ><Button bsStyle="info" href={'../cart-show/' + order.id}>SHOW</Button> <Button bsStyle="danger" href={'../cart-show/' + order.id}>CANCEL</Button> {order.description}, customer: {order.customer ? order.customer.name : ""} <Badge>Delivered</Badge> <Badge>{order.createdDate}</Badge><Badge>Total: {order.total}</Badge></ListGroupItem>;
-                }
-                return res;
-              })
-            }
+            {this.state.orders.map(order => {
+              return <ListGroupItem key={order.id}>
+                  <Button bsStyle="info" href={"../cart-show/" + order.id}>
+                  <Glyphicon glyph="open" />{" "}
+                  </Button>
+                  <Button onClick={() => this.removeOrder(order.id)} bsStyle="danger">
+                    <Glyphicon glyph="remove" />{" "}
+                  </Button>
+                  {order.description}, customer: {order.customer ? order.customer.name : ""}
+                  <Badge>{order.createdDate}</Badge>
+                  <Badge>Total: {order.total}</Badge>
+                </ListGroupItem>;
+            })}
           </ListGroup>
         </Grid>
       </div>
@@ -46,7 +79,7 @@ class CartListComponent extends React.Component {
   }
 }
 
-CartListComponent.displayName = 'CartCartListComponent';
+CartListComponent.displayName = "CartCartListComponent";
 
 // Uncomment properties you need
 // CartListComponent.propTypes = {};
